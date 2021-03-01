6428e0f7bfcbe4bd6ed81b320638291d0c5ceb59
epndavis <47059262+epndavis@users.noreply.github.com>
2021-03-01 22:19:35 +0000

    Feat: Add configuration

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -8,11 +8,15 @@ class PackageServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/package.php', 'package'
+        );
     }
 
     public function boot()
     {
-
+        $this->publishes([
+            __DIR__ . '/../config/package.php' => config_path('package.php'),
+        ]);
     }
 }
